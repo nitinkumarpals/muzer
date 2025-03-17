f76583199df7546ce68ed515e545b47329dbd226
nitinkumarpals <nitinpal900@gmail.com>
2025-03-17 15:25:58 +0530

    Add Prisma integration with schema setup and update package dependencies

--- src/components/Appbar.tsx
+++ src/components/Appbar.tsx
@@ -1,14 +1,35 @@
 "use client";
 import { Button } from "./ui/button";
-import { signIn } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 const Appbar = () => {
+  const {data: session, status} = useSession();
+
+
   return (
     <div className="m-4">
       <div className="flex justify-between">
         <div>Muzer</div>
         <div>
-          <Button className="bg-blue-300 cursor-pointer" onClick={() => signIn()}>Sign in</Button>
+          {status === "loading" ? (
+            <Button className="bg-gray-300 cursor-not-allowed" disabled>
+              Loading...
+            </Button>
+          ) : session?.user?.name ? (
+            <Button
+              className="bg-red-300 cursor-pointer"
+              onClick={() => signOut()}
+            >
+              Logout
+            </Button>
+          ) : (
+            <Button
+              className="bg-blue-300 cursor-pointer"
+              onClick={() => signIn()}
+            >
+              Sign in
+            </Button>
+          )}
         </div>
       </div>
     </div>
