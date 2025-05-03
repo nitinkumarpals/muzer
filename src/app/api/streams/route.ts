b5e63627268038876e26e1a8deda061d66cb3432
nitinkumarpals <nitinpal900@gmail.com>
2025-05-03 13:35:30 +0530

    removed log

--- src/app/api/streams/route.ts
+++ src/app/api/streams/route.ts
@@ -35,7 +35,6 @@ export async function POST(req: NextRequest) {
     const extractedId = new URL(result.data.url).searchParams.get("v") ?? "";
 
     const youtubeResult = await youtubesearchapi.GetVideoDetails(extractedId);
-    console.log(youtubeResult);
     if (!youtubeResult || !youtubeResult.thumbnail || !youtubeResult.thumbnail.thumbnails) {
       return NextResponse.json(
         { error: "Invalid request", message: "Unable to fetch video details" },
